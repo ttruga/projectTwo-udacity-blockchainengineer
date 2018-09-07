c811ffbbd30f1c41f0ef3619eb6ea6c456639ba3
Pablo Castro <ttruga@yahoo.com>
2018-09-07 12:02:44 -0300

    Implemented last two endpoints for getting stars by address and by hash

--- index.js
+++ index.js
@@ -1,184 +1,206 @@
-const express        = require('express');
-const bodyParser     = require('body-parser');
-const bitcoinMessage = require('bitcoinjs-message');
-const Blockchain     = require('./simpleChain').Blockchain;
-const path           = require('path');
+const express        = require('express')
+const bodyParser     = require('body-parser')
+const bitcoinMessage = require('bitcoinjs-message')
+const Blockchain     = require('./simpleChain').Blockchain
+const path           = require('path')
 
-const app   = express();
-const chain = new Blockchain();
+const app   = express()
+const chain = new Blockchain()
 
-const memoryValidations = {};
-const windowDuration = 300;
+const memoryValidations = {}
+const windowDuration    = 300
 
 // Configuring the express framework to use the json parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 /**
  * GET block endpoint
  * Receives height as parameter
  */
 app.get('/block/:height', (req, res) => {
-  const { height } = req.params;
+  const { height } = req.params
   if (height) {
     return chain.getBlock(height)
-    .then(block => {
-      if (block) {
-        return res.status(200).send(block);
-      }
-      return res.status(400).send({ message: 'no block available at this height' });
-    });
+      .then(block => {
+        if (block) {
+          return res.status(200).send(block)
+        }
+        return res.status(400).send({ message: 'no block available at this height' })
+      })
   }
-  res.status(400).send({ message: 'no block height available' });
-});
+  res.status(400).send({ message: 'no block height available' })
+})
 
 /**
  * POST block endpoint
  * Receives a block object as parameter inside body
  */
 app.post('/block', (req, res) => {
-  const { body } = req;
+  const { body } = req
   if (!body) {
-    return res.status(400).send({ message: 'block is required' });
+    return res.status(400).send({ message: 'block is required' })
   }
 
-  const { address, star } = req.body;
+  const { address, star } = req.body
 
   if (!address) {
-    return res.status(400).send({ message: 'Invalid request, [address] missing' });
+    return res.status(400).send({ message: 'Invalid request, [address] missing' })
   }
 
   if (!star) {
-    return res.status(400).send({ message: 'Invalid request, [star] missing' });
+    return res.status(400).send({ message: 'Invalid request, [star] missing' })
   }
 
-  const validationEntry = memoryValidations[address];
+  const validationEntry = memoryValidations[address]
 
   if (!validationEntry) {
-    return res.status(400).send({ message: 'address needs to get authorization first, go to /requestAuthorization' });
+    return res.status(400).send({ message: 'address needs to get authorization first, go to /requestAuthorization' })
   }
 
   if (!validationEntry.registerStar) {
-    return res.status(400).send({ message: 'star registration not valid for this address' });
+    return res.status(400).send({ message: 'star registration not valid for this address' })
   }
 
-  const newBlock = { address, star };
+  // TODO: ENCODEAR EL "STORY" DENTRO DE LA STAR
+  const newBlock = { body: { address, star } }
 
-  return chain.addBlock(newBlock)             // we store the block
-  .then(() => chain.getBlockHeight())
-  .then(height => chain.getBlock(height - 1)) // we retrieve the new block
-  .then(block => {
-    console.log('New block: ', block);
-    delete memoryValidations[address];
-    return res.status(200).send(block);       // we return the new block
-  });
-});
+  return chain.addBlock(newBlock) // we store the block
+    .then(() => chain.getBlockHeight())
+    .then(height => chain.getBlock(height - 1)) // we retrieve the new block
+    .then(block => {
+      console.log('New block: ', block)
+      delete memoryValidations[address]
+      return res.status(200).send(block) // we return the new block
+    })
+})
 
 app.post('/requestValidation', (req, res) => {
-  const { address } = req.body;
+  const { address } = req.body
 
   if (!address) {
-    return res.status(400).send({ message: 'Invalid request, blockchain address missing' });
+    return res.status(400).send({ message: 'Invalid request, blockchain address missing' })
   }
 
-  const addressEntry     = memoryValidations[address];
-  const requestTimestamp = Math.floor(Date.now() / 1000);
+  const addressEntry = memoryValidations[address]
+  const requestTimestamp = Math.floor(Date.now() / 1000)
 
   if (!addressEntry) {
-    //memoryValidations[address] = { timestamp: requestTimestamp, message: `${address}:${requestTimestamp}:starRegistry` };
-    memoryValidations[address] = { timestamp: requestTimestamp, message: '142BDCeSGbXjWKaAnYXbMpZ6sbrSAo3DpZ:1532330740:starRegistry' };
+    // todo: DESCOMENTAR ESTA LINEA Y BORRAR LA SIGUIENTE PARA QUE PUEDA USARSE CON CUALQUIER LLAVE
+    // memoryValidations[address] = { timestamp: requestTimestamp, message: `${address}:${requestTimestamp}:starRegistry` };
+    memoryValidations[address] = {
+      timestamp: requestTimestamp,
+      message: '142BDCeSGbXjWKaAnYXbMpZ6sbrSAo3DpZ:1532330740:starRegistry'
+    }
   }
 
-  const window           = Math.floor(Date.now() / 1000) - memoryValidations[address].timestamp;
-  const validationWindow = windowDuration - window;
+  const window = Math.floor(Date.now() / 1000) - memoryValidations[address].timestamp
+  const validationWindow = windowDuration - window
 
   if (validationWindow < 0) {
-    delete memoryValidations[address];
-    return res.status(400).send({ message: 'time window expired' });
+    delete memoryValidations[address]
+    return res.status(400).send({ message: 'time window expired' })
   }
 
-  const message = memoryValidations[address].message;
+  const message = memoryValidations[address].message
 
-  return res.status(200).send({ message, requestTimestamp, validationWindow });
-});
+  return res.status(200).send({ message, requestTimestamp, validationWindow })
+})
 
 app.post('/message-signature/validate', (req, res) => {
-  const { address, signature } = req.body;
+  const { address, signature } = req.body
 
   if (!address) {
-    return res.status(400).send({ message: 'Invalid request, [address] missing' });
+    return res.status(400).send({ message: 'Invalid request, [address] missing' })
   }
 
   if (!signature) {
-    return res.status(400).send({ message: 'Invalid request, [signature] missing' });
+    return res.status(400).send({ message: 'Invalid request, [signature] missing' })
   }
 
-  const validationEntry = memoryValidations[address];
+  const validationEntry = memoryValidations[address]
   if (!validationEntry) {
-    return res.status(400).send({ message: 'validation data not found! retry with new request, go again to /requestValidation' });
+    return res.status(400).send({ message: 'validation data not found! retry with new request, go again to /requestValidation' })
   }
 
-  const requestTimestamp = Math.floor(Date.now() / 1000);
+  const requestTimestamp = Math.floor(Date.now() / 1000)
 
-  const window = Math.floor(Date.now() / 1000) - validationEntry.timestamp;
-  const validationWindow = windowDuration - window;
+  const window = Math.floor(Date.now() / 1000) - validationEntry.timestamp
+  const validationWindow = windowDuration - window
 
   if (validationWindow < 0) {
-    delete memoryValidations[address];
-    return res.status(400).send({ message: 'validation window expired, go again to /requestValidation' });
+    delete memoryValidations[address]
+    return res.status(400).send({ message: 'validation window expired, go again to /requestValidation' })
   }
 
-  const message = validationEntry.message;
+  const message = validationEntry.message
 
-  let registerStar = null;
+  let registerStar = null
 
   try {
-    registerStar = bitcoinMessage.verify(message, address, signature);
-  } catch(error) {
-    console.error(error);
-    return res.status(400).send({ error: error.message });
+    registerStar = bitcoinMessage.verify(message, address, signature)
+  } catch (error) {
+    console.error(error)
+    return res.status(400).send({ error: error.message })
   }
 
   if (!registerStar) {
-    const status = { address, requestTimestamp, message, validationWindow, messageSignature: 'invalid' };
-    return res.status(400).send({ registerStar, status });
+    const status = { address, requestTimestamp, message, validationWindow, messageSignature: 'invalid' }
+    return res.status(400).send({ registerStar, status })
   }
 
-  memoryValidations[address].registerStar = registerStar;
-  const status = { address, requestTimestamp, message, validationWindow, messageSignature: 'valid' };
-  return res.status(200).send({ registerStar, status });
-});
-
+  memoryValidations[address].registerStar = registerStar
+  const status = {
+    address,
+    requestTimestamp,
+    message,
+    validationWindow,
+    messageSignature: 'valid'
+  }
+  return res.status(200).send({ registerStar, status })
+})
 
 app.get('/stars/address:address', (req, res) => {
-  const { address } = req.params;
-  console.log(address);
-  if(address[0] !== ':'){
-    return res.status(400).send({ error: `address with wrong format: [${address}]` });
+  const { address } = req.params
+  console.log(address)
+
+  if (address[0] !== ':') {
+    return res.status(400).send({ error: `address with wrong format: [${address}]` })
   }
 
-  return res.status(200).send({ address });
-});
+  return chain.getBlocksByAddress(address.substring(1))
+    .then(blocks => {
+      if (blocks) {
+        return res.status(200).send(blocks)
+      }
+      return res.status(400).send({ message: 'no registered stars found for this address' })
+    })
+})
 
 app.get('/stars/hash:hash', (req, res) => {
-  const { hash } = req.params;
-  console.log(hash);
-  if(hash[0] !== ':'){
-    return res.status(400).send({ error: `hash with wrong format: [${hash}]` });
+  const { hash } = req.params
+  console.log(hash)
+
+  if (hash[0] !== ':') {
+    return res.status(400).send({ error: `hash with wrong format: [${hash}]` })
   }
 
-  return res.status(200).send({ address });
-});
-
-
+  return chain.getBlockByHash(hash.substring(1))
+    .then(block => {
+      if (block) {
+        return res.status(200).send(block)
+      }
+      return res.status(400).send({ message: 'no registered stars found for this hash' })
+    })
+})
 
 /**
  * root basepath serves the documentation
  */
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
+  res.sendFile(path.join(`${__dirname}/index.html`))
+})
 
 app.listen(8000, () => {
-  console.log(`server running on port 8000`);
-});
+  console.log(`server running on port 8000`)
+})
--- simpleChain.js
+++ simpleChain.js
@@ -1,134 +1,173 @@
-const SHA256 = require('crypto-js/sha256');
-const level  = require('level');
+const SHA256 = require('crypto-js/sha256')
+const level = require('level')
 
 class Block {
-  constructor(data) {
-    this.hash   = '';
-    this.height = 0;
-    this.body   = data;
-    this.time   = 0;
-    this.previousBlockHash = '';
+  constructor (data) {
+    this.hash   = ''
+    this.height = 0
+    this.body   = data
+    this.time   = 0
+    this.previousBlockHash = ''
   }
 }
 
 class Blockchain {
-  constructor() {
-    const chainDB = './chaindata';
-    this.chain    = level(chainDB);
+  constructor () {
+    const chainDB = './chaindata'
+    this.chain = level(chainDB)
     this.getBlockHeight()
-    .then(blockHeight => {
-      console.log(`Current chain height: ${blockHeight}`);
-      if (blockHeight === 0) {
-        return this.addBlock(new Block('First block in the chain - Genesis block'));
-      }
-    });
+      .then(blockHeight => {
+        console.log(`Current chain height: ${blockHeight}`)
+        if (blockHeight === 0) {
+          return this.addBlock(new Block('First block in the chain - Genesis block'))
+        }
+      })
   }
 
-  getBlockHeight() {
+  getBlockHeight () {
     return new Promise((resolve, reject) => {
-      let blockIndex = 0;
+      let blockIndex = 0
       this.chain.createReadStream()
-      .on('data', () => {
-        blockIndex++;
-      })
-      .on('error', err => {
-        console.log(`getBlockHeight error: ${err.message}`);
-        reject(err);
-      })
-      .on('close', () => {
-        resolve(blockIndex);
-      });
-    });
+        .on('data', () => {
+          blockIndex++
+        })
+        .on('error', err => {
+          console.log(`getBlockHeight error: ${err.message}`)
+          reject(err)
+        })
+        .on('close', () => {
+          resolve(blockIndex)
+        })
+    })
   }
 
-  async addBlock(newBlock) {
-    let blockHeight = 0;
+  async addBlock (newBlock) {
+    let blockHeight = 0
     try {
-      blockHeight = await this.getBlockHeight();
+      blockHeight = await this.getBlockHeight()
       if (blockHeight === 0) {
-        newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
-        return this.chain.put(blockHeight, JSON.stringify(newBlock));
+        newBlock.hash = SHA256(JSON.stringify(newBlock)).toString()
+        return this.chain.put(blockHeight, JSON.stringify(newBlock))
       }
 
-      const lastBlock = await this.getBlock(blockHeight - 1);
-      newBlock.height            = blockHeight;
-      newBlock.time              = new Date().getTime().toString().slice(0, -3);
-      newBlock.previousBlockHash = lastBlock.hash;
-      newBlock.hash              = SHA256(JSON.stringify(newBlock)).toString();
+      const lastBlock            = await this.getBlock(blockHeight - 1)
+      newBlock.height            = blockHeight
+      newBlock.time              = new Date().getTime().toString().slice(0, -3)
+      newBlock.previousBlockHash = lastBlock.hash
+      newBlock.hash              = SHA256(JSON.stringify(newBlock)).toString()
 
-      return this.chain.put(newBlock.height, JSON.stringify(newBlock));
-
-    } catch(err) {
+      return this.chain.put(newBlock.height, JSON.stringify(newBlock))
+    } catch (err) {
       if (err) {
-        console.error('Block ' + blockHeight + ' submission failed', err);
-        return null;
+        console.error('Block ' + blockHeight + ' submission failed', err)
+        return null
       }
     }
   }
 
-  async getBlock(blockIndex) {
+  async getBlock (blockIndex) {
     try {
-      const block = await this.chain.get(blockIndex);
+      const block = await this.chain.get(blockIndex)
       if (block) {
-        return JSON.parse(block);
+        return JSON.parse(block)
       }
-      return null;
-    } catch(e) {
-      console.log(`getBlock err:  ${e.message}`);
-      return null;
+      return null
+    } catch (e) {
+      console.log(`getBlock err:  ${e.message}`)
+      return null
     }
   }
 
-  async validateBlock(blockHeight) {
-    const block = await this.getBlock(blockHeight);
+  async getBlocksByAddress (address) {
+    const height = await this.getBlockHeight()
+    const blocks = []
+    for (let i = 0; i < height; i++) {
+      try {
+        if (i !== 0) {
+          let block = await this.getBlock(i)
+          console.log(block)
+          if (block && block.body && block.body.address === address) {
+            //TODO: DECODEAR EL "STORY" DEL STAR ANTES DE GUARDAR
+            blocks.push(block)
+          }
+        }
+      } catch (e) {
+        console.log('validateChain error: ', e.message)
+      }
+    }
+
+    return blocks
+  }
+
+  async getBlockByHash (hash) {
+    const height = await this.getBlockHeight()
+    for (let i = 0; i < height; i++) {
+      try {
+        if (i !== 0) {
+          let block = await this.getBlock(i)
+          if (block && block.hash && block.hash === hash) {
+            //TODO: DECODEAR EL "STORY" DEL STAR ANTES DE RETORNAR
+            return block
+          }
+        }
+      } catch (e) {
+        console.log('validateChain error: ', e.message)
+      }
+    }
+
+    return null
+  }
+
+  async validateBlock (blockHeight) {
+    const block = await this.getBlock(blockHeight)
     if (block) {
-      let blockHash = block.hash;
-      block.hash = '';
-      let validBlockHash = SHA256(JSON.stringify(block)).toString();
+      let blockHash = block.hash
+      block.hash = ''
+      let validBlockHash = SHA256(JSON.stringify(block)).toString()
 
       if (blockHash === validBlockHash) {
-        console.log('Block #' + blockHeight + ' is VALID!');
-        return true;
+        console.log('Block #' + blockHeight + ' is VALID!')
+        return true
       } else {
-        console.log('Block #' + blockHeight + ' IS INVALID!!! hash:\n' + blockHash + ' <> ' + validBlockHash);
-        return false;
+        console.log('Block #' + blockHeight + ' IS INVALID!!! hash:\n' + blockHash + ' <> ' + validBlockHash)
+        return false
       }
     }
-    return null;
+    return null
   }
 
-  async validateChain() {
-    let errorLog = [];
+  async validateChain () {
+    let errorLog = []
 
-    const height = await this.getBlockHeight();
+    const height = await this.getBlockHeight()
 
     for (let i = 0; i < height; i++) {
-      const blockIsValid = await this.validateBlock(i);
+      const blockIsValid = await this.validateBlock(i)
 
       if (!blockIsValid) {
-        errorLog.push(i);
+        errorLog.push(i)
       }
 
       try {
         if (i !== 0) {
-          let block         = await this.getBlock(i);
-          let previousBlock = await this.getBlock(i - 1);
+          let block = await this.getBlock(i)
+          let previousBlock = await this.getBlock(i - 1)
           if (block.previousBlockHash !== previousBlock.hash) {
-            errorLog.push(i);
+            errorLog.push(i)
           }
 
           if (errorLog.length > 0) {
-            console.log('Block errors = ' + errorLog.length);
-            console.log('Blocks: ' + errorLog);
+            console.log('Block errors = ' + errorLog.length)
+            console.log('Blocks: ' + errorLog)
           } else {
-            console.log('validateChain: No errors detected');
+            console.log('validateChain: No errors detected')
           }
         }
-      } catch(e) {
-        console.log('validateChain error: ', e.message);
+      } catch (e) {
+        console.log('validateChain error: ', e.message)
       }
     }
   }
 }
 
-module.exports = {Block, Blockchain};
+module.exports = { Block, Blockchain }
